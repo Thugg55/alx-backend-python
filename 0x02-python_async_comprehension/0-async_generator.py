bb2e57014a7c03f301d11d359d2e3afc80d6372a
Thugg55 <thuggeru7@gmail.com>
2024-10-19 02:02:53 +0000

    corrction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,14 @@
 
 import asyncio
 import random
+from typing import Generator 
 
 
-aync def async_generator():
+async def async_generator():
+    """
+     Asynchronous generator function that yields a random float between 0 and 10
+    after a one second delay for a total of 10 iterations.
+    """
     for _in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
